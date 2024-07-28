ebffd95da4ce7e71722626b30f51988672bf1e6b
Annie Huang <annie.hn@gmail.com>
2024-07-28 14:37:56 +1000

    Create ApolloClient. Seems a very lengthly way to create the client, not sure if it is because it's a 2020 tutorial.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,25 @@
 import './App.css';
+import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, location, path }) =>
+      alert(`GraphQL error: ${message}`)
+    );
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: 'http://localhost:6969/graphql' }),
+]);
+
+// Seems a very lengthly way to create the client, not sure if it is because it's a 2020 tutorial.
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function App() {
   return <></>;
